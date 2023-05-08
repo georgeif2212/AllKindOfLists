eda117f7e85d86a38b96da74f547d811d06159c0
georgeif2212 <2203025256@cua.uam.mx>
2023-05-08 10:06:48 -0600

    Cambio variables

--- ListasCirculares/mainLC.c
+++ ListasCirculares/mainLC.c
@@ -11,11 +11,11 @@ int main()
   Nodo *aux1, *aux2;
   L = crear_ListaSC();
 
-  insertar_cabeza(L, crear_Nodo(crear_Alumno(1, "areli", 25)));
-  insertar_cabeza(L, crear_Nodo(crear_Alumno(2, "rojo", 25)));
-  insertar_cabeza(L, crear_Nodo(crear_Alumno(3, "hndz", 25)));
-  insertar_cola(L, crear_Nodo(crear_Alumno(4, "mario", 25)));
-  insertar_enmedio(L, aux1, crear_Nodo(crear_Alumno(7, "torresSSSSS", 28)));
+  insertar_cabeza(L, crear_Nodo(crear_Alumno(1, "Jorge", 25)));
+  insertar_cabeza(L, crear_Nodo(crear_Alumno(2, "Infante", 25)));
+  insertar_cabeza(L, crear_Nodo(crear_Alumno(3, "Fragoso", 25)));
+  insertar_cola(L, crear_Nodo(crear_Alumno(4, "Pedro", 25)));
+  insertar_enmedio(L, aux1, crear_Nodo(crear_Alumno(7, "Mariano", 28)));
   printLista(L);
 
   quitar_cola(L);
